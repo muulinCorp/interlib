cc1ce3d88b295d8e05d8d92de35fb76c194ac311
Peter Chen <ch.focke@gmail.com>
2022-09-23 11:11:38 +0800

    feat: add mock auth middle

---
+++ core/mid/mock_auth.go
@@ -0,0 +1,37 @@
+package mid
+
+import (
+	"strings"
+
+	"github.com/94peter/sterna/api/mid"
+	"github.com/94peter/sterna/auth"
+	"github.com/94peter/sterna/util"
+	"github.com/gin-gonic/gin"
+)
+
+func NewMockAuthMid() mid.GinMiddle {
+	return &mockAuthMiddle{}
+}
+
+type mockAuthMiddle struct {
+}
+
+func (lm *mockAuthMiddle) GetName() string {
+	return "mockAuth"
+}
+
+func (am *mockAuthMiddle) Handler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Request = util.SetCtxKeyVal(
+			c.Request,
+			auth.CtxUserInfoKey,
+			auth.NewReqUser(
+				util.GetHost(c.Request),
+				c.GetHeader("Mock_User_UID"),
+				c.GetHeader("Mock_User_ACC"),
+				c.GetHeader("Mock_User_NAM"),
+				strings.Split(c.GetHeader("Mock_User_Roles"), ","),
+			))
+		c.Next()
+	}
+}
