95d92c90cc41540246b73a684ffd0b052d5311fa
Chen Yan Jui <ch.focke@gmail.com>
2024-07-09 14:26:35 +0800

    feat: add routine data stream client

--- com2scada/client/com2scada.go
+++ com2scada/client/com2scada.go
@@ -60,3 +60,55 @@ func (impl *changeDataStreamImpl) StartChangeDataStream(fields []string, resp ch
 func (impl *changeDataStreamImpl) StopStream() error {
 	return impl.stream.CloseSend()
 }
+
+type RoutineDataStreamClient interface {
+	StartRoutineDataStream(fields []string, resp chan map[string]float64) error
+	StopStream() error
+}
+
+func NewRoutineDataStreamClient(address string) RoutineDataStreamClient {
+	return &routineDataStreamImpl{
+		address: address,
+	}
+}
+
+type routineDataStreamImpl struct {
+	address string
+	stream  pb.Com2ScadaService_RoutineDataStreamClient
+}
+
+func (impl *routineDataStreamImpl) StartRoutineDataStream(fields []string, resp chan map[string]float64) error {
+	var err error
+	ctx := context.Background()
+	grpcClt, err := grpc_tool.NewConnection(ctx, impl.address)
+	if err != nil {
+		return err
+	}
+	defer grpcClt.Close()
+	service := pb.NewCom2ScadaServiceClient(grpcClt)
+	impl.stream, err = service.RoutineDataStream(ctx, &pb.FieldList{Fields: fields})
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			in, err := impl.stream.Recv()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				return
+			}
+			resp <- in.Values
+		}
+	}()
+	wg.Wait()
+	return nil
+}
+
+func (impl *routineDataStreamImpl) StopStream() error {
+	return impl.stream.CloseSend()
+}
